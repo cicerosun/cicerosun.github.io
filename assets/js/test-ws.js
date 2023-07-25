47062f98bbc72bb0c38c61a8634ce77db5ad7420
cicerosun <cicerosun69@gmail.com>
2023-07-25 11:55:16 +0800

    Update test-ws.js

--- assets/js/test-ws.js
+++ assets/js/test-ws.js
@@ -1,28 +1,28 @@
 // 创建WebSocket对象，指定WSS协议的URL
-const socket = new WebSocket('ws://gmve-qa2.gamematrix.qq.com/inst/conn');
+const socket1 = new WebSocket('ws://gmve-qa2.gamematrix.qq.com/inst/conn');
 
 // 监听连接事件
-socket.addEventListener('open', (event) => {
+socket1.addEventListener('open', (event) => {
   console.log('WebSocket连接已建立');
   
   // 发送消息到后端服务器
-  socket.send('Hello, backend server!');
+  socket1.send('Hello, backend server!');
 });
 
 // 监听接收消息事件
-socket.addEventListener('message', (event) => {
+socket1.addEventListener('message', (event) => {
   console.log('收到消息：', event.data);
   
   // 关闭WebSocket连接
-  socket.close();
+  socket1.close();
 });
 
 // 监听关闭连接事件
-socket.addEventListener('close', (event) => {
+socket1.addEventListener('close', (event) => {
   console.log('WebSocket连接已关闭');
 });
 
 // 监听错误事件
-socket.addEventListener('error', (event) => {
+socket1.addEventListener('error', (event) => {
   console.error('WebSocket连接发生错误：', event);
 });
